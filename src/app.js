fcd2c53522a8bd9afd1e7c312b924082835cbd90
Luis Ortiz <luisangel930115@gmail.com>
2026-02-18 13:48:31 -0500

    feat: página de bienvenida en ruta raíz

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const cors = require('cors');
+const cors    = require('cors');
+const path    = require('path');
 
-const routes = require('./routes');
+const routes      = require('./routes');
 const errorHandler = require('./shared/middleware/error.middleware');
 
 const app = express();
@@ -9,8 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', routes);
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
 
+app.use('/api', routes);
 app.use(errorHandler);
 
 module.exports = app;
